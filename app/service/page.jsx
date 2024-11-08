f72c1ce1ee0530dc7c84b39bec2d2c2da0fa1a6a
riverkite0708 <0908xmrqkr@gmail.com>
2024-11-08 12:10:00 +0900

    [feat/#9]: 탭메뉴 구현

--- app/service/page.jsx
+++ app/service/page.jsx
@@ -1,18 +1,50 @@
 import ButtonL from '@/components/service/common/Button/ButtonL';
 import ButtonM from '@/components/service/common/Button/ButtonM';
 import ButtonS from '@/components/service/common/Button/ButtonS';
+import TabMenu from '@/components/service/common/TabMenu/TabMenu';
+import { Box, Flex, Strong, Text } from '@radix-ui/themes';
+import { tabMenuList } from '@/data/tabMenuList/service';
+import styles from './page.module.css';
+
 export default function Service() {
   return (
     <div>
       <h1>서비스 페이지</h1>
-      버튼L
-      <ButtonL style="deepYellow">버튼1</ButtonL>
-      <ButtonL style="lightYellow">버튼2</ButtonL>
-      버튼M
-      <ButtonM leftText="M1" rightText="M2" />
-      버튼S
-      <ButtonS style="deepYellow">s1</ButtonS>
-      <ButtonS style="lightYellow">s2</ButtonS>
+      <Flex direction="column" gap="10px" className={styles.container}>
+        <Flex direction="column" gap="10px" asChild>
+          <section>
+            <div className={styles.title}>
+              <Strong>/components/service/common/Button</Strong>
+            </div>
+            <div className={styles.content}>
+              <Box>
+                <Text as="p">버튼L</Text>
+                <ButtonL style="deepYellow">버튼1</ButtonL>
+                <ButtonL style="lightYellow">버튼2</ButtonL>
+              </Box>
+              <Box>
+                <Text as="p">버튼M</Text>
+                <ButtonM leftText="M1" rightText="M2" />
+              </Box>
+              <Box>
+                <Text as="p">버튼S</Text>
+                <ButtonS style="deepYellow">s1</ButtonS>
+                <ButtonS style="lightYellow">s2</ButtonS>
+              </Box>
+            </div>
+          </section>
+        </Flex>
+        <Flex direction="column" gap="10px" asChild>
+          <section>
+            <div className={styles.title}>
+              <Strong>/components/service/common/TabMenu</Strong>
+            </div>
+            <div className={styles.content}>
+              <TabMenu tabMenuList={tabMenuList} />
+            </div>
+          </section>
+        </Flex>
+      </Flex>
     </div>
   );
 }
---
+++ components/service/common/TabMenu/TabMenu.jsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+import styles from './TabMenu.module.css';
+import { TabNav, Text } from '@radix-ui/themes';
+
+export default function TabMenu({ tabMenuList }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const url = searchParams.size > 0 ? `${pathname}?${searchParams}` : pathname;
+
+  return (
+    <div className={styles.tab_menu}>
+      <TabNav.Root>
+        {tabMenuList.map((tab) => {
+          return (
+            <TabNav.Link asChild active={url === tab.href} key={`tab_${tab.text}`}>
+              <Link href={tab.href}>
+                <Text as="p" size="3" weight="medium">
+                  {tab.text}
+                </Text>
+              </Link>
+            </TabNav.Link>
+          );
+        })}
+      </TabNav.Root>
+    </div>
+  );
+}
