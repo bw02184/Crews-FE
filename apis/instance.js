bb9439b17ac99dafa43b332bbf4dc9b5878d8cf9
riverkite0708 <0908xmrqkr@gmail.com>
2024-11-10 16:52:42 +0900

    [feat/#10]: instance 설정

---
+++ apis/instance.js
@@ -0,0 +1,40 @@
+const fetchInstance = async (url, options) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    ...options.headers,
+  };
+
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL + 'api/vi';
+
+  try {
+    const response = await fetch(`${BASE_URL}${url}`, {
+      ...options,
+      headers,
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      if (response.status === 403) {
+        console.error('Token Expired');
+      }
+      console.error('Fetch Error:', errorResponse);
+      return response;
+    }
+
+    if (response.headers.get('Content-Type')?.includes('application/json')) await response.json();
+    else return await response.text();
+  } catch (error) {
+    console.log('Fetch Error:', error);
+    throw error;
+  }
+};
+
+const instance = {
+  get: (url, options) => fetchInstance(url, { ...options, method: 'GET' }),
+  post: (url, options) => fetchInstance(url, { ...options, method: 'POST' }),
+  put: (url, options) => fetchInstance(url, { ...options, method: 'PUT' }),
+  patch: (url, options) => fetchInstance(url, { ...options, method: 'PATCH' }),
+  delete: (url, options) => fetchInstance(url, { ...options, method: 'DELETE' }),
+};
+
+export default instance;
