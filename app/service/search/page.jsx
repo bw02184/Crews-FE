2a33311535909176e2a31a02b26819620898b75e
kimchang0 <newshfkfk@gmail.com>
2024-11-15 17:39:29 +0900

    [Feat#23]: Search_Page_Append

--- app/service/search/page.jsx
+++ app/service/search/page.jsx
@@ -1,3 +1,43 @@
+import { Box, Flex, RadioGroup, Text } from '@radix-ui/themes';
+import { Header, SelectFilter } from '@/components/common';
+import { searchMenu } from '@/constants/selectMenuList/searchMenuList';
+import Image from 'next/image';
+import styles from './page.module.css';
+
 export default function Search() {
-  return <div>검색 페이지</div>;
+  return (
+    <div className="page">
+      <Header side="left">검색하기</Header>
+      <div className="content">
+        <section>
+          <Flex align="center" gap="2" asChild>
+            {/* 셀렉트 박스 */}
+            <form className={styles.search_form}>
+              <SelectFilter filter="keyword" selectList={searchMenu}>
+                모임명
+              </SelectFilter>
+              {/* 검색 입력창 */}
+              <input
+                type="text"
+                id="search_input"
+                placeholder="키워드를 입력해주세요!"
+                className="search-input"
+              ></input>
+              <button type="submit">
+                <Image src="/icons/search_icon.svg" width={15} height={15} alt="검색하기" />
+              </button>
+              <Text as="p" weight="medium" mb="1">
+                최근 검색어
+              </Text>
+              {/* <ButtonS
+                type="submit"
+                style="right"
+                icon={{ src: '/icons/search_icon.svg', width: '15', height: '15', alt: '삭제' }}
+              ></ButtonS> */}
+            </form>
+          </Flex>
+        </section>
+      </div>
+    </div>
+  );
 }
---
+++ constants/selectMenuList/searchMenuList.js
@@ -0,0 +1,4 @@
+export const searchMenu = [
+  { text: '모임명', params: 'name' },
+  { text: '한줄소개', params: 'introduce' },
+];
