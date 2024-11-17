b30c097abba24fc415cc2412d7658b97925f5fda
riverkite0708 <0908xmrqkr@gmail.com>
2024-11-17 20:08:18 +0900

    마이페이지 롤백

--- app/service/mypage/page.jsx
+++ app/service/mypage/page.jsx
@@ -1,25 +1,3 @@
-'use client';
-
-import { useSessionStatusStore } from '@/stores/authStore';
-import { signOut } from 'next-auth/react';
-
 export default function Mypage() {
-  const { setSessionStatus } = useSessionStatusStore();
-  return (
-    <div className="page">
-      <div className="content">
-        <section>
-          <button
-            onClick={async () => {
-              await signOut();
-              setSessionStatus(false);
-              alert('로그아웃 되었습니다!');
-            }}
-          >
-            로그아웃
-          </button>
-        </section>
-      </div>
-    </div>
-  );
+  return <div>마이페이지</div>;
 }
