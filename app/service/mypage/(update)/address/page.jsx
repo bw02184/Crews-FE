b8da3857c86a7dd771d468f45520485cd520e973
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-17 14:59:33 +0900

    [refact/#17]: Section 태그 section 태그로 수정

--- app/service/mypage/(update)/address/page.jsx
+++ app/service/mypage/(update)/address/page.jsx
@@ -1,17 +1,16 @@
 import { Header } from '@/components/common';
 import AddressInputBox from '@/components/mypage/address/AddressInputBox';
 import BottomButton from '@/components/mypage/bottombutton/BootomButton';
-import { Section } from '@radix-ui/themes';
 
 export default function page() {
   return (
     <div className="page">
       <Header side="center">활동지역 수정</Header>
       <div className="content">
-        <Section>
+        <section>
           <AddressInputBox />
           <BottomButton />
-        </Section>
+        </section>
       </div>
     </div>
   );
