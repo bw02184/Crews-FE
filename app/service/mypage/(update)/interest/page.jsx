8b7ca9efd38b7429964e4c0619dbdc8456e7e734
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-17 18:39:46 +0900

    [refact/#17] : Section -> section 수정

--- app/service/mypage/(update)/interest/page.jsx
+++ app/service/mypage/(update)/interest/page.jsx
@@ -1,15 +1,14 @@
 import { Header } from '@/components/common';
 import InterestForm from '@/components/mypage/interest/InterestForm';
-import { Section } from '@radix-ui/themes';
 
 export default function page() {
   return (
     <div className="page">
       <Header side="center">관심사 수정</Header>
       <div className="content">
-        <Section>
+        <section>
           <InterestForm />
-        </Section>
+        </section>
       </div>
     </div>
   );
