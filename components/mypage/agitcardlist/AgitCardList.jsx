ead8a3556b38ef7b20abb7e638a234fd88e5eccb
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-17 18:41:42 +0900

    [feat/#17]: 이미지 카드 <Link>로  변경

--- components/mypage/agitcardlist/AgitCardList.jsx
+++ components/mypage/agitcardlist/AgitCardList.jsx
@@ -1,20 +1,22 @@
 'use client';
 
 import Sortdown from '../sortdowns/Sortdowns';
+import Link from 'next/link';
 
 const { ImageCard } = require('@/components/common');
 const { Flex } = require('@radix-ui/themes');
 
 export default function AgitCardList({ data }) {
-  const handlerAgit = () => {
-    alert('버튼이 클릭되었습니다.');
-  };
   return (
     <>
       <Sortdown />
       <Flex direction="column" gap="10px">
         {data.map((agit, i) => {
-          return <ImageCard data={agit} key={`agit${i}`} onClick={handlerAgit}></ImageCard>;
+          return (
+            <Link href={`/agits/${agit.agitId}`} key={`agit${i}`}>
+              <ImageCard data={agit} />
+            </Link>
+          );
         })}
       </Flex>
     </>
