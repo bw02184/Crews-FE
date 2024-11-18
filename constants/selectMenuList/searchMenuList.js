b816a3513345792730fdd818100044a111b04791
kimchang0 <newshfkfk@gmail.com>
2024-11-18 17:53:56 +0900

    [Feat#23]: SearchMenuList_Create

--- constants/selectMenuList/searchMenuList.js
+++ constants/selectMenuList/searchMenuList.js
@@ -2,3 +2,18 @@ export const searchMenu = [
   { text: '모임명', params: 'name' },
   { text: '한줄소개', params: 'introduce' },
 ];
+
+export const sortSelectNameList = [
+  { text: '이름 오름차순', params: 'asc' },
+  { text: '이름 내림차순', params: 'desc' },
+];
+
+export const sortSelectDateList = [
+  { text: '최근 날짜순', params: 'asc' },
+  { text: '오래된 날짜순', params: 'desc' },
+];
+
+export const sortSelectPersonList = [
+  { text: '사람 적은순', params: 'asc' },
+  { text: '사람 많은순', params: 'desc' },
+];
