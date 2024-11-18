832274018dd8b2029510ad808026112c6c92e767
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-18 17:48:41 +0900

    [feat/#17]: 자산관리 컴포넌트 병합

--- app/service/mypage/(profile)/assets/page.jsx
+++ app/service/mypage/(profile)/assets/page.jsx
@@ -1,3 +1,12 @@
+import AgitCard from '@/components/mypage/assets/AgitCard';
+import MyAccount from '@/components/mypage/assets/MyAccount';
+
 export default function Page() {
-  return <div>자산관리 컴포넌트</div>;
+  return (
+    <div>
+      자산관리 컴포넌트
+      <AgitCard></AgitCard>
+      <MyAccount></MyAccount>
+    </div>
+  );
 }
