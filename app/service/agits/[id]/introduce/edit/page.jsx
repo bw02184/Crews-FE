4dd6598c5c143ddeecfc888bbcdb68fe9d7d71ff
riverkite0708 <0908xmrqkr@gmail.com>
2024-11-22 15:07:03 +0900

    [style] checkboxLabel -> interest_list로 common css에 설정

--- app/service/agits/[id]/introduce/edit/page.jsx
+++ app/service/agits/[id]/introduce/edit/page.jsx
@@ -4,7 +4,6 @@ import { SelectFilter, TabMenu, Title, Label, ButtonL } from '@/components/commo
 import { Flex, Box, Text } from '@radix-ui/themes';
 import { tabMenuList } from '@/constants/tabMenuList/agits';
 import { agitsSelectMenuList } from '@/constants/selectMenuList/sample';
-import styles from './page.module.css';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 
@@ -111,123 +110,92 @@ export default function Page({ params }) {
                 <Text as="label" className="require">
                   관심사
                 </Text>
-                <Flex gap="10px" wrap="wrap" asChild>
-                  <ul>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-1"
-                        value="등산"
-                        checked={selectedInterests.includes(1)}
-                        onChange={() => toggleInterest(1)}
-                      />
-                      <label htmlFor="interest-1" className={styles.checkboxLabel}>
-                        등산
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-2"
-                        value="조기축구회"
-                        checked={selectedInterests.includes(2)}
-                        onChange={() => toggleInterest(2)}
-                      />
-                      <label htmlFor="interest-2" className={styles.checkboxLabel}>
-                        조기축구회
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-3"
-                        value="해양 스포츠"
-                        checked={selectedInterests.includes(3)}
-                        onChange={() => toggleInterest(3)}
-                      />
-                      <label htmlFor="interest-3" className={styles.checkboxLabel}>
-                        해양 스포츠
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-4"
-                        value="클라이밍"
-                        checked={selectedInterests.includes(4)}
-                        onChange={() => toggleInterest(4)}
-                      />
-                      <label htmlFor="interest-4" className={styles.checkboxLabel}>
-                        클라이밍
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-5"
-                        value="피트니스"
-                        checked={selectedInterests.includes(5)}
-                        onChange={() => toggleInterest(5)}
-                      />
-                      <label htmlFor="interest-5" className={styles.checkboxLabel}>
-                        피트니스
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-6"
-                        value="러닝"
-                        checked={selectedInterests.includes(6)}
-                        onChange={() => toggleInterest(6)}
-                      />
-                      <label htmlFor="interest-6" className={styles.checkboxLabel}>
-                        러닝
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-7"
-                        value="요가 및 필라테스"
-                        checked={selectedInterests.includes(7)}
-                        onChange={() => toggleInterest(7)}
-                      />
-                      <label htmlFor="interest-7" className={styles.checkboxLabel}>
-                        요가 및 필라테스
-                      </label>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        className={styles.checkboxInput}
-                        id="interest-8"
-                        value="격투기"
-                        checked={selectedInterests.includes(8)}
-                        onChange={() => toggleInterest(8)}
-                      />
-                      <label htmlFor="interest-8" className={styles.checkboxLabel}>
-                        격투기
-                      </label>
-                    </li>
-                    {errors.interests && (
-                      <Text as="p" className="error">
-                        {errors.interests.message}
-                      </Text>
-                    )}
-                  </ul>
-                </Flex>
-
-                <Text as="p" size="2" weight="medium" className="gray_t1" mt="5px">
-                  최소 1개에서 3개까지 등록할 수 있습니다!
-                </Text>
+                <div className="interest_list">
+                  <Flex gap="10px" wrap="wrap" asChild>
+                    <ul>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-1"
+                          checked={selectedInterests.includes(1)}
+                          onChange={() => toggleInterest(1)}
+                        />
+                        <label htmlFor="interest-1">등산</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-2"
+                          checked={selectedInterests.includes(2)}
+                          onChange={() => toggleInterest(2)}
+                        />
+                        <label htmlFor="interest-2">조기축구회</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-3"
+                          checked={selectedInterests.includes(3)}
+                          onChange={() => toggleInterest(3)}
+                        />
+                        <label htmlFor="interest-3">해양 스포츠</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-4"
+                          checked={selectedInterests.includes(4)}
+                          onChange={() => toggleInterest(4)}
+                        />
+                        <label htmlFor="interest-4">클라이밍</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-5"
+                          checked={selectedInterests.includes(5)}
+                          onChange={() => toggleInterest(5)}
+                        />
+                        <label htmlFor="interest-5">피트니스</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-6"
+                          checked={selectedInterests.includes(6)}
+                          onChange={() => toggleInterest(6)}
+                        />
+                        <label htmlFor="interest-6">러닝</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-7"
+                          checked={selectedInterests.includes(7)}
+                          onChange={() => toggleInterest(7)}
+                        />
+                        <label htmlFor="interest-7">요가 및 필라테스</label>
+                      </li>
+                      <li>
+                        <input
+                          type="checkbox"
+                          id="interest-8"
+                          checked={selectedInterests.includes(8)}
+                          onChange={() => toggleInterest(8)}
+                        />
+                        <label htmlFor="interest-8">격투기</label>
+                      </li>
+                      {errors.interests && (
+                        <Text as="p" className="error">
+                          {errors.interests.message}
+                        </Text>
+                      )}
+                    </ul>
+                  </Flex>
+                  <Text as="p" size="2" weight="medium" className="gray_t1" mt="5px">
+                    최소 1개에서 3개까지 등록할 수 있습니다!
+                  </Text>
+                </div>
               </Box>
               <ButtonL style="deep" type="submit">
                 수정하기
