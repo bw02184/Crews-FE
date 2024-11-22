2eeaded3a8b8f5dddd9411e275ace6cbe0cd7bec
kimchang0 <newshfkfk@gmail.com>
2024-11-22 10:02:06 +0900

    [Fix#37]: Agit_Create_Done_Page_Fix

--- app/service/agits/create/done/page.jsx
+++ app/service/agits/create/done/page.jsx
@@ -9,19 +9,23 @@ export default async function Page() {
       <Header side="left">생성 완료</Header>
       <div className="content">
         <Flex direction="column" gap="10px" className="content">
-          <Flex direction="column" gap="10px" asChild align="center">
+          <Flex direction="column" gap="20px" asChild align="center">
             <section>
-              <Box as="div" content="image">
-                <Image src="/imgs/img_complete.png" width={100} height={100} alt={`완료 이미지`} />
+              <Box>
+                <Image src="/imgs/img_complete.png" width={130} height={130} alt={`완료 이미지`} />
               </Box>
-              <Box as="div" content="text" className={`${styles.texts} gray_t1`}>
-                <Title>아지트 생성 완료!</Title>
-                <Text as="p" weight="light">
-                  모임 통장을 지금 개설하시겠습니까? <i className="dpb"></i> ※ 이미 사용중인 계좌 연결은 아지트 홈에서
-                  가능합니다.
-                </Text>
+              <Box content="text" align="center">
+                <Box>
+                  <Title>아지트 생성 완료!</Title>
+                </Box>
+                <Box className=" gray_t1" mt="5%">
+                  <Text as="p" weight="light" size="14px">
+                    모임 통장을 지금 개설하시겠습니까? <i className="dpb"></i> ※ 이미 사용중인 계좌 연결은 아지트 홈에서
+                    가능합니다.
+                  </Text>
+                </Box>
               </Box>
-              <Box as="div" content="button" className={styles.buttons}>
+              <Box content="button" className={styles.buttons}>
                 <ButtonM
                   leftButton={{ as: 'link', text: '나중에', href: `/service/agits` }}
                   rightButton={{ as: 'link', text: '생성하기', href: `/service/agits` }}
