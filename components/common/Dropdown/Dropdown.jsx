6768a956094a482372910776f79fabba7ada4e65
bw02184 <bw02184@naver.com>
2024-11-25 16:39:03 +0900

    fix: 삭제된 파일 복원

--- components/common/Dropdown/Dropdown.jsx
+++ components/common/Dropdown/Dropdown.jsx
@@ -1,5 +1,36 @@
 'use client';
 
+import { useState } from 'react';
+import styles from './Dropdown.module.css';
+import { Box } from '@radix-ui/themes';
+import Link from 'next/link';
+
 export default function Dropdown({ as = 'button', side = 'left', menuList, children }) {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Box className={`${styles.dropdown} popover`}>
+      <div
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {children}
+      </div>
+      {isOpen && (
+        <ul className={side == 'right' ? styles.right : ''}>
+          {menuList.map((menu, i) => {
+            return (
+              <li key={`dropdown${i}`}>
+                {as == 'button' ? (
+                  <button onClick={menu.onClick}>{menu.text}</button>
+                ) : (
+                  <Link href={menu.href}>{menu.text}</Link>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </Box>
+  );
 }
