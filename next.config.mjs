2a37a2f399be955156b72fd71e6937901e47ffad
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-12-03 17:05:02 +0900

    refactor/#52: 미사용 설정 제거(개인 S3)

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'hwamockyee.s3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
