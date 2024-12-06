a99bef0bb302a3da86e0db1c45d4e498618a1c4a
riverkite0708 <0908xmrqkr@gmail.com>
2024-12-06 12:32:00 +0900

    [fix]: ProfileCard 경로 대소문자 수정

--- app/service/mypage/(profile)/layout.jsx
+++ app/service/mypage/(profile)/layout.jsx
@@ -1,6 +1,6 @@
 import { getProfile } from '@/apis/mypageAPI';
 import { Header, TabMenu } from '@/components/common';
-import ProfileCard from '@/components/mypage/ProfileCard/ProfileCard';
+import ProfileCard from '@/components/mypage/Profilecard/ProfileCard';
 import { tabMenuList } from '@/constants/tabMenuList/mypage';
 
 export default async function Layout({ children }) {
